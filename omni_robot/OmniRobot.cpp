e48dbcf7fe6bf3541940096b121d6fadc16edba8
FWachter <wachterfreddy@gmail.com>
2018-02-14 21:52:49 -0500

    Cleaned up files

--- omni_robot/OmniRobot.cpp
+++ omni_robot/OmniRobot.cpp
@@ -9,20 +9,16 @@
 
 /* PUBLIC FUNCTIONS */
 bool OmniRobot::isSetup() {
-
 	return setup;
-
 }
 
 /* PRIVATE FUNCTIONS */
 void OmniRobot::_setupMotorPins() {
-
 	pinMode(_pin_pwm_motor_left,  OUTPUT);
 	pinMode(_pin_pwm_motor_right, OUTPUT);
 	pinMode(_pin_pwm_motor_tail,  OUTPUT);
 	pinMode(_pin_ppm_motor_shoot, OUTPUT);
 	pinMode(_pin_ppm_motor_push,  OUTPUT);
-
 }
 
 
